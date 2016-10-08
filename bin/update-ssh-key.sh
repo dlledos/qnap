ef4119c36a1517ae344adb76ca4cda377795e061
Didier Lledos <didier.lledos@gmail.com>
2016-10-08 18:12:34 +0200

    ADD: commentaire

--- bin/update-ssh-key.sh
+++ bin/update-ssh-key.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
 SSHKEY="/root/.ssh/authorized_keys"
+# La clé se trouve dans C:\Users\Did\.ssh\id_rsa.ppk à ouvrir avec PuTTYgen
+SSHKEY_VALUE="/share/CACHEDEV1_DATA/homes/admin/id_rsa_windows10"
+
 grep "update-ssh-key.sh" $SSHKEY
 if [ ! $? -eq 0 ]
 then
 	echo "# ADDDED by update-ssh-key.sh" >> $SSHKEY 
-	echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEApLEPS/nptP7oCHM43i5tswCbG4gG18Ie11E5a57z+UwHKJTXflxXCPKUwdUcL2fmJDNV3WNbfyeOi0b4nLXOsk9i3N18AQMYiPv69HEVLh87/rtPqyUgjbml8c2PNDpqCAVoR5cOLFXgM8U1eMm5/2VOFPBzmYQNAHtJnC85evcNE3ERI5qHPDCuWhx/GMZ8ze2FKA1fyk4fEiWTr/7Dhy1cl3DjUbn4lfML5dk+qJ+Rg2NBi5kPtxUtDXvB4o3irynYLEnCSnkQvcnMJ6nYtTYf64/pTCxKNVypbslfM4JrF8JgviTrpHX0k77ZLyYuFjqnl2GfzkPCU7ZW8ZzqTw== rsa-key-20160425"  >> $SSHKEY 
+	cat $SSHKEY_VALUE  >> $SSHKEY 
 fi
