5d57dd40889058ffcf8675a4f3d6705674a69961
Didier Lledos <didier.lledos@gmail.com>
2016-10-08 22:25:27 +0200

    Amélioration

---
+++ bin/rename-and-add-S00E00.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+MOTIF=$1
+REMPLACE=$2
+S=1
+E=1
+
+
+IFS=$'\n'
+for I in $(find . -type f -regex ".*$MOTIF.*" | grep -v ".@__thumb")
+do
+	echo $I
+	EP=$(printf "%02d" $E)
+	DEST=$(echo "$I" | sed -e "s/\(.*\)$MOTIF\(.*\)/\1$REMPLACE.S${S}E${EP}\2/")
+	E=$[ $E + 1 ]
+	echo mv "$I" "$DEST"
+	mv "$I" "$DEST"
+done
--- bin/tvshow-manage.sh
+++ bin/tvshow-manage.sh
@@ -6,8 +6,9 @@ DEST="/share/CACHEDEV1_DATA/Multimedia/Série"
 IFS=$'\n'
 for I in $(/opt/bin/find $SRC -type f -regex ".*[s|S][0-9]+[e|E][0-9]+.*[mkv|mp4|avi]$" | grep -v ".@__thumb")
 do
-	DIR=$(echo $I | /opt/bin/sed -e "s/.*[\/]\(.*\)[.|][s|S][0-9][0-9][e|E][0-9][0-9].*[mkv|mp4|avi]$/\1/")
+	FILE=$(echo $I | tr -s " " "." | tr -s "_" "." | tr -s "-" "." | sed -e "s/.*\/\(.*\)/\1/" | sed -e "s/^\.\(.*\)/\1/")
+	DIR=$(echo $FILE | sed -e "s/\(^.*\)\.[S|s].*/\1/")
 	mkdir -p "$DEST/$DIR"
-	echo mv "$I" "$DEST/$DIR"
-	mv "$I" "$DEST/$DIR"
+	echo mv "$I" "$DEST/$DIR/$FILE"
+	mv "$I" "$DEST/$DIR/$FILE"
 done
