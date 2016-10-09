003ecb16a679a299084eb43f38664ff663994143
Didier Lledos <didier.lledos@gmail.com>
2016-10-09 09:07:00 +0200

    ADD: update ssh-key bashrc and crontab to my-autorun.sh

--- bin/my-autorun.sh
+++ bin/my-autorun.sh
@@ -2,3 +2,7 @@
 
 # Start JDOWNLOADER2
 nohup /share/CACHEDEV1_DATA/.qpkg/jd2/opt/JDownloader2 &
+
+/share/CACHEDEV1_DATA/bin/update-ssh-key.sh
+/share/CACHEDEV1_DATA/bin/update-bashrc.sh
+/share/CACHEDEV1_DATA/bin/update-crontab.sh
