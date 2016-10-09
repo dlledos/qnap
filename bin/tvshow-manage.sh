17cde4a58daf4d2e94f1d17cdbf4cb17fa22d480
Didier Lledos <didier.lledos@gmail.com>
2016-10-09 21:20:31 +0200

    FIX: Dir

--- bin/tvshow-manage.sh
+++ bin/tvshow-manage.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+PATH=/opt/bin:$PATH
 date
 SRC=${1:-.}
 DEST="/share/CACHEDEV1_DATA/Multimedia/Série"
 IFS=$'\n'
-for I in $(/opt/bin/find $SRC -type f -regex ".*[s|S][0-9]+[e|E][0-9]+.*[mkv|mp4|avi]$" | grep -v ".@__thumb")
+for I in $(find $SRC -type f -regex ".*[s|S][0-9]+[e|E][0-9]+.*[mkv|mp4|avi]$" | grep -v ".@__thumb")
 do
 	FILE=$(echo $I | tr -s " " "." | tr -s "_" "." | tr -s "-" "." | sed -e "s/.*\/\(.*\)/\1/" | sed -e "s/^\.\(.*\)/\1/")
-	DIR=$(echo $FILE | sed -e "s/\(^.*\)\.[S|s].*/\1/")
+	DIR=$(echo $FILE | sed -e "s/\(^.*\)\.\([S|s][0-9][0-9][E|e][0-9][0-9].*\)/\1/")
 	mkdir -p "$DEST/$DIR"
 	echo mv "$I" "$DEST/$DIR/$FILE"
 	mv "$I" "$DEST/$DIR/$FILE"
