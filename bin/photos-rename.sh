c0668ac97da81ae833232e1abe5b1ebb1b3c52d5
didier Lledos <didier.lledos@orange.com>
2016-10-14 15:42:15 +0200

    boucle

--- bin/photos-rename.sh
+++ bin/photos-rename.sh
@@ -2,7 +2,7 @@
 
 PHOTOS_DIR="/share/CACHEDEV1_DATA/Multimedia/Camera-à-trier"
 
-for PHOTO in `find . -regex "[mp4|jpg]"`
-  echo $PHOTO
+for PHOTO in $(find . -regex "[mp4|jpg])
+  echo "$PHOTO"
 do
 #exiftool
