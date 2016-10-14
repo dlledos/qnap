96c4f2027f0b21d9acb6eb71344df41ce25c82f8
Didier Lledos <didier.lledos@gmail.com>
2016-10-14 10:59:05 +0200

    Toutes les 10mins

--- bin/update-crontab.sh
+++ bin/update-crontab.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-mkdir -p /share/CACHEDEV1_DATA/log
-crontab -l > /tmp/crontab.list
-grep "tvshow-manage.sh" /tmp/crontab.list
+DESTINATION="/tmp/crontab.list"
+LOG_DIR="/share/CACHEDEV1_DATA/log"
+LOG="$LOG_DIR/crontab.log"
+mkdir -p $LOG_DIR
+crontab -l > $DESTINATION
+SCRIPT=$(basename $0)
+grep "$SCRIPT"  $DESTINATION
 if [ ! $? -eq 0 ]
 then
-	echo "* * * * * /share/CACHEDEV1_DATA/bin/tvshow-manage.sh /share/CACHEDEV1_DATA/Multimedia/jdownloader >> /share/CACHEDEV1_DATA/log/crontab.log  2>> /share/CACHEDEV1_DATA/log/crontab.log" >> /tmp/crontab.list
-	crontab /tmp/crontab.list
+	echo "# ADDED BY $SCRIPT" >>  $DESTINATION
+	echo "1,11,21,31,41,51 * * * * /share/CACHEDEV1_DATA/bin/tvshow-manage.sh /share/CACHEDEV1_DATA/Multimedia/jdownloader >> $LOG 2>> $LOG" >> $DESTINATION
+	crontab $DESTINATION
 fi
