c9d0bfbda13208a47bfa0d12b45d058fa3559511
Didier Lledos <didier.lledos@gmail.com>
2016-10-17 19:43:56 +0200

    Tests

--- bin/test/test-tvshow-manage.sh
+++ bin/test/test-tvshow-manage.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
 
-BIN="/home/dlledos/src/qnap/bin/tvshow-manage.sh"
-TEST_DIR=$(cd $(dirname $0) ; pwd)
-RESOURCES_DIR="$TEST_DIR/resources/$(basename $0)"
+BIN="/share/CACHEDEV1_DATA/bin/tvshow-manage.sh"
+ROOT_DIR=$(cd $(dirname $0) ; pwd)
+RESOURCES_DIR="$ROOT_DIR/resources/$(basename $0)"
 
 
 function myTest() {
-    TEST=$1
-    FILE=$2
-    EXPECTED=$3
-    rm -rf "$RESOURCES_DIR/$TEST"
+    TEST="$1"
+    FILE="$2"
+    EXPECTED="$3"
+    TEST_DIR="$RESOURCES_DIR/$TEST"
+	rm -rf "$RESOURCES_DIR"
     
-    echo "# Testing $TEST"
-    FILENAME="$RESOURCES_DIR/$FILE"
-    touch $FILENAME
+	mkdir -p "$TEST_DIR"
+    FILENAME="$TEST_DIR/$FILE"
+    touch "$FILENAME"
     
-    $BIN "$RESOURCES_DIR" "$RESOURCES_DIR/$TEST"
+    "$BIN" "$RESOURCES_DIR" "$TEST_DIR" 2>/dev/null
     
-    echo $(checkFileExist "$RESOURCES_DIR/$TEST/$EXPECTED")
+	RESULT=$(checkFileExist "$TEST_DIR/$EXPECTED")
+    echo "# Testing $TEST :" $RESULT
+	if [ "$RESULT" = "KO" ]
+	then
+		echo "  -> expected : $TEST_DIR/$EXPECTED"
+		echo "  -> but got  : $TEST_DIR/$(ls $TEST_DIR)"    
+	fi
 }
 
 function checkFileExist(){
@@ -27,10 +34,13 @@ function checkFileExist(){
     then
         echo "OK"
     else
-        echo "KO"    
+        echo "KO"
     fi
 }
 
-echo "### Testing $BIN with resources $RESOURCES_DIR ###"
 myTest "nominal" "machin.S01E01.truc.avi" "machin/machin.S01E01.truc.avi"
-#myTest "StartWithS00E00" "S01E01.truc.avi"
+myTest "espace-dans-fichier" "machin S01E01 truc avi" "machin/machin.S01E01.truc.avi"
+myTest "commence-avec-un-point" ".machin S01E01 truc avi" "machin/machin.S01E01.truc.avi"
+myTest "commence-avec-un-tiret" "-machin S01E01 truc avi" "machin/machin.S01E01.truc.avi"
+myTest "nombre-sur-2-chiffres" "-machin S1E1 truc avi" "machin/machin.S01E01.truc.avi"
+myTest "StartWithS01E01" "S01E01.truc.avi" ""
--- bin/tvshow-manage.sh
+++ bin/tvshow-manage.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 PATH=/opt/bin:$PATH
-date
 SRC=${1:-.}
 DEST=${2:-"/share/CACHEDEV1_DATA/Multimedia/Série"}
 IFS=$'\n'
@@ -9,13 +8,13 @@ for I in $(find $SRC -type f -regex ".*[s|S][0-9]+[e|E][0-9]+.*[mkv|mp4|avi]$" |
 do
 	FILE=$(echo $I | tr -s " " "." | tr -s "_" "." | tr -s "-" "." | sed -e "s/.*\/\(.*\)/\1/" | sed -e "s/^\.\(.*\)/\1/")
 	DIR=$(echo $FILE | sed -e "s/\(^.*\)\.\([S|s][0-9]*[E|e][0-9].*\)/\1/")
-	RES=$(echo $FILE | sed -e "s/\(.*)[S|s]\([0-9]*\)[E|e]\([0-9]*\)\(\.*\)/\1 \2 \3 \4/")
+	RES=$(echo $FILE | sed -e "s/\(.*\)[S|s]\([0-9]*\)[E|e]\([0-9]*\)\(\.*\)/\1 \2 \3 \4/")
 	DEBUT=$(echo "$RES" | cut -d " " -f1)
 	SAISON=$(printf "%02d" $(echo $((10#$(echo "$RES" | cut -d " " -f2)))))
 	EPISODE=$(printf "%02d" $(echo $((10#$(echo "$RES" | cut -d " " -f3)))))
 	FIN=$(echo "$RES" | cut -d " " -f4)
 	FILE="${DEBUT}S${SAISON}E$EPISODE$FIN"
 	mkdir -p "$DEST/$DIR"
-	echo mv "$I" "$DEST/$DIR/$FILE"
+	echo mv "$I" "$DEST/$DIR/$FILE" >&2
 	mv "$I" "$DEST/$DIR/$FILE"
 done
