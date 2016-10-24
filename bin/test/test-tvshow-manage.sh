b8a2242d0c1fcdd825af37992feb9d653ca08f12
Didier Lledos <didier.lledos@gmail.com>
2016-10-24 09:08:57 +0200

    FIX

--- bin/test/test-tvshow-manage.sh
+++ bin/test/test-tvshow-manage.sh
@@ -11,20 +11,19 @@ function myTest() {
     FILE="$2"
     EXPECTED="$3"
     TEST_DIR="$RESOURCES_DIR/$TEST"
-	rm -rf "$RESOURCES_DIR"
-    
-	mkdir -p "$TEST_DIR"
+    rm -rf "$RESOURCES_DIR"
+    mkdir -p "$TEST_DIR"
     FILENAME="$TEST_DIR/$FILE"
     touch "$FILENAME"
-    
+
     "$BIN" "$RESOURCES_DIR" "$TEST_DIR" 2>/dev/null
-    
+
 	RESULT=$(checkFileExist "$TEST_DIR/$EXPECTED")
     echo "# Testing $TEST :" $RESULT
 	if [ "$RESULT" = "KO" ]
 	then
 		echo "  -> expected : $TEST_DIR/$EXPECTED"
-		echo "  -> but got  : $TEST_DIR/$(ls $TEST_DIR)"    
+		echo "  -> but got  : $TEST_DIR/$(ls $TEST_DIR)/$(cd $TEST_DIR/$(ls $TEST_DIR); ls)"
 	fi
 }
 
@@ -44,6 +43,5 @@ myTest "commence-avec-un-point" ".machin S01E01 truc avi" "machin/machin.S01E01.
 myTest "commence-avec-un-tiret" "-machin S01E01 truc avi" "machin/machin.S01E01.truc.avi"
 myTest "nombre-sur-2-chiffres" "-machin S1E1 truc avi" "machin/machin.S01E01.truc.avi"
 myTest "se-dans-le-texte-reste-se" "machin.S01E01.se.avi" "machin/machin.S01E01.se.avi"
-#Steven Universe S02E03(55) ou S01E48 - La visite surprise d'Oncle Grandpa (Say Uncle).mp4
 myTest "deux-fois-SxxExx-prends-le-1er" "machin.S02E03.truc.S01E48.bidule.mp4" "machin/machin.S02E03.truc.S01E48.bidule.mp4"
 myTest "parenthèse-après-SxxExx" "machin.S02E03(1).truc.mp4" "machin/machin.S02E03.(1).truc.mp4"
--- bin/tvshow-manage.sh
+++ bin/tvshow-manage.sh
@@ -7,13 +7,17 @@ IFS=$'\n'
 for I in $(find $SRC -type f -regex ".*[s|S][0-9]+[e|E][0-9]+.*[mkv|mp4|avi]$" | grep -v ".@__thumb")
 do
 	FILE=$(echo $I | tr -s " " "." | tr -s "_" "." | tr -s "-" "." | sed -e "s/.*\/\(.*\)/\1/" | sed -e "s/^\.\(.*\)/\1/")
-	DIR=$(echo $FILE | sed -e "s/\(^.*\)\.\([S|s][0-9]*[E|e][0-9].*\)/\1/")
-	RES=$(echo $FILE | sed -e "s/\(.*\)[S|s]\([0-9]*\)[E|e]\([0-9]*\)\(\.*\)/\1 \2 \3 \4/")
-	DEBUT=$(echo "$RES" | cut -d " " -f1)
-	SAISON=$(printf "%02d" $(echo $((10#$(echo "$RES" | cut -d " " -f2)))))
-	EPISODE=$(printf "%02d" $(echo $((10#$(echo "$RES" | cut -d " " -f3)))))
-	FIN=$(echo "$RES" | cut -d " " -f4)
-	FILE="${DEBUT}S${SAISON}E$EPISODE$FIN"
+	#DIR=$(echo $FILE | sed -e "s/\(^.*\)\.\([S|s][0-9]\+[E|e][0-9]\+.*\)/\1/")
+	RES=$(echo $FILE | perl -ne 'chomp;print scalar reverse . "\n";' | sed -e "s/\(.*\.\)\([0-9]\+[E|e][0-9]\+[S|s]\)\.\(.*\)/\1 \2 \3/")
+	echo $RES
+	#RES=$(echo $FILE | sed -e "s/\(.*\)[S|s]\([0-9]\+\)[E|e]\([0-9]\+\)\(.*\)/\1 \2 \3 \4/")
+	DEBUT=$(echo "$RES" | cut -d " " -f3 | perl -ne 'chomp;print scalar reverse . "\n";')
+	SE=$(echo "$RES" | cut -d " " -f2 | perl -ne 'chomp;print scalar reverse . "\n";')
+	SAISON=$(printf "%02d" $(echo $((10#$(echo "$SE" | sed -e "s/[S|s]\([0-9]\+\)[E|e]\([0-9]\+\)/\1/")))))
+	EPISODE=$(printf "%02d" $(echo $((10#$(echo "$SE" | sed -e "s/[S|s]\([0-9]\+\)[E|e]\([0-9]\+\)/\2/")))))
+	FIN=$(echo "$RES" | cut -d " " -f1 | perl -ne 'chomp;print scalar reverse . "\n";')
+	FILE="${DEBUT}.S${SAISON}E$EPISODE$FIN"
+	DIR=$DEBUT
 	mkdir -p "$DEST/$DIR"
 	echo mv "$I" "$DEST/$DIR/$FILE" >&2
 	mv "$I" "$DEST/$DIR/$FILE"
