ad133ad929bf09bc423c5004fae9ddc6513f5013
Didier Lledos <didier.lledos@gmail.com>
2016-10-29 22:26:50 +0200

    FIX PATH

--- bin/update-bashrc.sh
+++ bin/update-bashrc.sh
@@ -7,7 +7,7 @@ if [ ! $? -eq 0 ]
 then
 	echo "# ADDDED by update-bashrc.sh " >> $BASHRC 
 	echo "export JAVA_HOME=/usr/local/jre" >> $BASHRC
-	echo "export PATH=/opt/bin:$PATH:/share/CACHEDEV1_DATA/bin/:$JAVA_HOME/bin" >> $BASHRC 
+	echo "export PATH=/opt/bin:$PATH:/share/CACHEDEV1_DATA/bin/:\$JAVA_HOME/bin" >> $BASHRC 
 	echo "git config --global user.email didier.lledos@gmail.com" >> $BASHRC 
 	echo "git config --global user.name \"Didier Lledos\"" >> $BASHRC 
 	echo "cd /share/CACHEDEV1_DATA/Multimedia/jdownloader" >> $BASHRC 
