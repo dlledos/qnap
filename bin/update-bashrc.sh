96fff2adc3a9ac6bb728b8b9acfd476d09e6c0e2
Didier Lledos <didier.lledos@gmail.com>
2016-10-29 22:23:54 +0200

    ADD JAVA_HOME and git config

--- bin/update-bashrc.sh
+++ bin/update-bashrc.sh
@@ -6,7 +6,10 @@ grep "update-bashrc.sh" $BASHRC
 if [ ! $? -eq 0 ]
 then
 	echo "# ADDDED by update-bashrc.sh " >> $BASHRC 
-	echo "export PATH=/opt/bin:$PATH:/share/CACHEDEV1_DATA/bin/" >> $BASHRC 
+	echo "export JAVA_HOME=/usr/local/jre" >> $BASHRC
+	echo "export PATH=/opt/bin:$PATH:/share/CACHEDEV1_DATA/bin/:$JAVA_HOME/bin" >> $BASHRC 
+	echo "git config --global user.email didier.lledos@gmail.com" >> $BASHRC 
+	echo "git config --global user.name \"Didier Lledos\"" >> $BASHRC 
 	echo "cd /share/CACHEDEV1_DATA/Multimedia/jdownloader" >> $BASHRC 
 	source $BASHRC
 fi
