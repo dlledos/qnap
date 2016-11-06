f8577533fad6c3e6fae8c23eb7d6ed74dd7c9314
Didier Lledos <didier.lledos@gmail.com>
2016-11-06 22:07:59 +0100

    FIX: lacnement

--- bin/my-autorun.sh
+++ bin/my-autorun.sh
@@ -2,7 +2,7 @@
 
 # Start JDOWNLOADER2
 export PATH=/opt/bin:$PATH
-nohup /share/CACHEDEV1_DATA/.qpkg/jd2/opt/JDownloader2 &
+/opt/bin/nohup /share/CACHEDEV1_DATA/.qpkg/jd2/opt/JDownloader2 &
 
 /share/CACHEDEV1_DATA/bin/update-ssh-key.sh
 /share/CACHEDEV1_DATA/bin/update-bashrc.sh
